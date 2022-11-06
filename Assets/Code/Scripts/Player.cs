b9b94c31b0d2234cc126710fce0c42ad349aba7c
Jonathan ROGER <j.roger@boostmymail.com>
2022-11-06 15:21:00 +0100

    feat(InputSystem): activation du nouvel input system

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -5,14 +5,31 @@ namespace dcc
 {
     public class Player : NetworkBehaviour
     {
-        private NetworkVariable<int> shoot = new NetworkVariable<int>(1,
-            NetworkVariableReadPermission.Everyone,
-            NetworkVariableWritePermission.Owner);
+        [SerializeField] private bool _isOnline = true;
+        [SerializeField] private Rigidbody _rb;
+
+        [SerializeField] private InputAction _playerInputs; 
         private void Update()
         {
-            if (!IsOwner)
+            if (_isOnline && !IsOwner)
                 return;
             
+            GatherInput();
+            // SimpleMove();
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void GatherInput()
+        {
+            // read values  
+        }
+
+        private void SimpleMove()
+        {
             Vector3 moveDir = new Vector3(0, 0, 0);
             if (Input.GetKey(KeyCode.Z)) moveDir.z = +1f;
             if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
